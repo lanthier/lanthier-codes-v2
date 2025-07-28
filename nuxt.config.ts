66cd59b66d0ec025ddfdfcf4f5ee9568af43994a
Lanthier <lanthier72@gmail.com>
2025-07-28 08:33:11 -0600

    add favicon and support new domain

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,8 +21,15 @@ export default defineNuxtConfig({
 
   // GitHub Pages configuration
   app: {
-    baseURL: process.env.NODE_ENV === 'production' ? '/lanthier-codes-v2/' : '/',
     buildAssetsDir: '/_nuxt/',
+    head: {
+      link: [
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'manifest', href: '/site.webmanifest' }
+      ]
+    }
   },
 
   content: {
